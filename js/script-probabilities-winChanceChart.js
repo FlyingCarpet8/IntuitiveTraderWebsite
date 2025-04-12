3455a3afae22be950061fa886a655ddc5d071340
FlyingCarpet8 <110906496+FlyingCarpet8@users.noreply.github.com>
2025-04-12 15:06:18 +0100

    Update script-probabilities-winChanceChart.js

--- js/script-probabilities-winChanceChart.js
+++ js/script-probabilities-winChanceChart.js
@@ -4,7 +4,7 @@ const winChanceChart = new Chart(winChanceChartCtx, {
   data: {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
-      label: 'Sample Data',
+      label: 'Win Chance %',
       data: [12, 19, 3, 5, 2, 85],
       backgroundColor: 'rgba(75, 192, 192, 0.2)',
       borderColor: 'rgba(75, 192, 192, 1)',
